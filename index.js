290a2be9ed89b54677a2582ae04e985ae950c900
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2014-05-19 20:25:54 +0200

    simple setTimeout and tick
    	add tape

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 'use strict';
+
+var _ = require('lodash');
+
+function createClock() {
+  var clock = 0;
+  var savedCallbacks = {};
+  var callbackNum = 0;
+
+  return Object.freeze({
+    setTimeout: function(cb, delay) {
+      savedCallbacks[callbackNum++] = {
+        callback: cb,
+        startTime: clock,
+        delay: delay
+      };
+    },
+    tick: function(passedTime) {
+      clock += passedTime;
+      _(savedCallbacks).filter(function(savedCallback) {
+        return (savedCallback.startTime + savedCallback.delay) <= clock;
+      }).forEach(function(savedCallbacks) {
+        savedCallbacks.callback();
+      });
+    }
+  });
+}
+
+module.exports = createClock();
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,1 +1,23 @@
 'use strict';
+
+var test = require('tape');
+var mockTimeout = require('../');
+
+test('mockTimeout', function(t) {
+  t.equal(typeof mockTimeout.setTimeout, 'function', 'setTimeout should exist');
+  t.equal(typeof mockTimeout.tick, 'function', 'tick should exist');
+  t.ok(Object.isFrozen(mockTimeout), 'mockTimeout should be frozen');
+
+  test('setTimeout', function(t) {
+    t.plan(1);
+    mockTimeout.setTimeout(function() {
+      t.ok(true, 'timeout called the callback');
+    }, 10);
+
+    mockTimeout.setTimeout(function() {
+      t.ok(false, 'timeout should not called the callback');
+    }, 20);
+    mockTimeout.tick(10);
+  });
+  t.end();
+});
