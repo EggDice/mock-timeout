2ea85052511d36179d7f6a2e5bf04c3e32638f0a
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2014-08-25 15:01:22 +0200

    implemented setTimeout, setInterval, clearTimeout, clearInterval

--- index.js
+++ index.js
@@ -2,33 +2,84 @@
 
 var _ = require('lodash');
 
-function createClock() {
-  var clock = 0;
-  var savedCallbacks = {};
-  var callbackNum = 0;
+function createMock() {
+  var clock, savedCallbacks, callbackNum;
 
-  return Object.freeze({
-    reset: function() {
-      clock = 0;
-      savedCallbacks = {};
-      callbackNum = 0;
-    },
-    setTimeout: function(cb, delay) {
-      savedCallbacks[callbackNum++] = {
-        callback: cb,
+  function removeCallback(id) {
+    delete savedCallbacks[id];
+  }
+
+  function reset() {
+    clock = 0;
+    savedCallbacks = {};
+    callbackNum = 0;
+  }
+
+  function addCallback(isLooping) {
+    return function(cb, delay) {
+      savedCallbacks[callbackNum] = {
+        isLooping: isLooping,
+        fn: cb,
         startTime: clock,
         delay: delay
       };
-    },
-    tick: function(passedTime) {
-      clock += passedTime;
-      _(savedCallbacks).filter(function(savedCallback) {
-        return (savedCallback.startTime + savedCallback.delay) <= clock;
-      }).forEach(function(savedCallbacks) {
-        savedCallbacks.callback();
-      });
-    }
+      return callbackNum++;
+    };
+  }
+
+  function tick(passedTime) {
+    var interval = {
+      from: clock,
+      to: clock += passedTime
+    };
+    _(savedCallbacks).
+      filter(selectAffectedCallbacks(interval)).
+      forEach(callCallback(interval));
+  }
+
+  function selectAffectedCallbacks(timeInterval) {
+    return function(callback) {
+      var fireTime = callback.startTime + callback.delay;
+      return callback.isLooping || isInInterval(fireTime, timeInterval);
+    };
+  }
+
+  function isInInterval(point, interval) {
+    return point > interval.from && point <= interval.to;
+  }
+
+  function callCallback(timeInterval) {
+    return function (callback) {
+      _.times(
+        callback.isLooping ?
+          getTimesTheIntervalShouldCalled(callback, timeInterval) : 1,
+        callback.fn
+      );
+    };
+  }
+
+  function getTimesTheIntervalShouldCalled(callback, timeInterval) {
+    var elapsedTimeSinceStart = timeInterval.from - callback.startTime;
+    var elapsedTimeSinceLastCall = elapsedTimeSinceStart % callback.delay;
+    return Math.floor((duration(timeInterval) - elapsedTimeSinceLastCall) /
+        callback.delay);
+  }
+
+  function duration(interval) {
+    return interval.to - interval.from;
+  }
+
+  reset();
+
+  return Object.freeze({
+    reset: reset,
+    setTimeout: addCallback(false),
+    clearTimeout: removeCallback,
+    setInterval: addCallback(true),
+    clearInterval: removeCallback,
+    tick: tick
   });
 }
 
-module.exports = createClock();
+module.exports = createMock();
+
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,36 +1,94 @@
 'use strict';
 
+var _ = require('lodash');
 var test = require('tape');
 var mockTimeout = require('../');
 
+function genAssertCalled(t, shouldCall) {
+  return function() {
+    t.ok(shouldCall,
+        'timeout ' + (shouldCall ? '' : 'should not ') + 'called the callback');
+  };
+}
+
+function genAssertCallOnce(t) {
+  var called = false;
+  return function() {
+    t.ok(!called, called ? 'called more than once' : 'called once');
+    called = true;
+  };
+}
+
+function methodExist(t, name) {
+  t.equal(typeof mockTimeout[name], 'function', name + ' should exist');
+}
+
 test('mockTimeout', function(t) {
-  t.equal(typeof mockTimeout.setTimeout, 'function', 'setTimeout should exist');
-  t.equal(typeof mockTimeout.tick, 'function', 'tick should exist');
-  t.equal(typeof mockTimeout.reset, 'function', 'reset should exist');
+  [
+    'setTimeout', 'setInterval', 'tick', 'reset',
+    'clearTimeout', 'clearInterval'
+  ].forEach(_.partial(methodExist, t));
   t.ok(Object.isFrozen(mockTimeout), 'mockTimeout should be frozen');
 
   test('reset', function(t) {
-    mockTimeout.setTimeout(function() {
-      t.ok(false, 'timeout should not called the callback');
-    }, 10);
+    mockTimeout.setTimeout(genAssertCalled(t, false), 10);
     mockTimeout.reset();
     mockTimeout.tick(10);
     t.end();
   });
 
   test('setTimeout', function(t) {
-    t.plan(1);
+    t.plan(2);
     mockTimeout.reset();
-
-    mockTimeout.setTimeout(function() {
-      t.ok(true, 'timeout called the callback');
-    }, 10);
-
-    mockTimeout.setTimeout(function() {
-      t.ok(false, 'timeout should not called the callback');
-    }, 20);
-
+    mockTimeout.setTimeout(genAssertCalled(t, true), 10);
+    mockTimeout.setTimeout(genAssertCalled(t, false), 20);
     mockTimeout.tick(10);
+
+    mockTimeout.reset();
+    mockTimeout.setTimeout(genAssertCallOnce(t), 5);
+    mockTimeout.tick(10);
+    mockTimeout.tick(10);
+  });
+
+  test('setInterval', function(t) {
+    t.plan(15);
+    mockTimeout.reset();
+    mockTimeout.setInterval(genAssertCalled(t, true), 10);
+    mockTimeout.tick(40);
+
+    mockTimeout.reset();
+    mockTimeout.tick(5);
+    mockTimeout.setInterval(genAssertCalled(t, true), 10);
+    mockTimeout.tick(35);
+
+
+    mockTimeout.reset();
+    mockTimeout.tick(5);
+    mockTimeout.setInterval(genAssertCalled(t, true), 10);
+    mockTimeout.tick(40);
+
+
+    mockTimeout.reset();
+    mockTimeout.tick(5);
+    mockTimeout.setInterval(genAssertCalled(t, true), 10);
+    mockTimeout.tick(40);
+  });
+
+  test('clearTimeout', function(t) {
+    mockTimeout.reset();
+    var timeout = mockTimeout.setTimeout(genAssertCalled(t, false), 5);
+    mockTimeout.clearTimeout(timeout);
+    mockTimeout.tick(5);
+    t.end();
+  });
+
+  test('clearInterval', function(t) {
+    mockTimeout.reset();
+    var timeout = mockTimeout.setInterval(genAssertCalled(t, false), 5);
+    mockTimeout.clearInterval(timeout);
+    mockTimeout.tick(20);
+    t.end();
   });
   t.end();
 });
+
